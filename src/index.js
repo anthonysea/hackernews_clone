34836a0425d4afd159d5ec3e71ea2631c6bfd09a
anthonysea <anthonyqchao@gmail.com>
2020-03-05 20:59:17 -0800

    Gone through ch.1, wrote some notes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// Enable Hot Module Replacement (handles source code changes without refreshing)
+if (module.hot) {
+    module.hot.accept();
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
