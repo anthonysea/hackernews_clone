245231b4014c7728735e1f6e16aedf04fc94f42b
anthonysea <anthonyqchao@gmail.com>
2020-03-22 23:31:05 -0700

    Added basic error handling for API requests

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     }
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -79,7 +80,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
   // function callback when form input is submitted
@@ -115,7 +116,12 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey } = this.state;
+    const { 
+      searchTerm, 
+      results, 
+      searchKey,
+      error
+    } = this.state;
     const page = (
       results &&
       results[searchKey] &&
@@ -139,10 +145,16 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table 
-          list={list}
-          onDismiss={this.onDismiss}
-        />
+        { error
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+          : <Table 
+            list={list}
+            onDismiss={this.onDismiss}
+          />
+        }
+        
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
